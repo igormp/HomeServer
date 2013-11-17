f986dcd5201461938108c6641b8e8df12b9ceea1
Igor Moura <igorphilippini@hotmail.com>
2013-11-17 19:46:39 -0300

    Update nº 1
    Updated Email script
    Added Thanks page

--- email.php
+++ email.php
@@ -1,5 +1,5 @@
 <?php 
-if(isset($_POST['submit'])) { 
+{ 
 $to =  			"igorphilippini@hotmail.com"; 
 $subject =      $_POST['subject']; 
 $name_field =   $_POST['name']; 
@@ -8,9 +8,7 @@ $message =      $_POST['message'];
   
 $body =  "From: $name_field\n E-Mail: $email_field\n Subject: $subject\n Message:\n $message"; 
   
-echo "Your mail was sent, thanks :)"; 
 mail($to, $subject, $body);
-}  else { 
-echo "blarg!";
-} 
+header('Location: ty.html');
+}
 ?>
